11d6beb3c7dac7967e1ee4fb1d7ee345a4d54e5e
PAlbee <paul@albee.com>
2025-01-05 22:16:45 -0800

    Working version

--- MMM-BusTimesOBA.js
+++ MMM-BusTimesOBA.js
@@ -1,11 +1,10 @@
 // import Module from "node:module"
 
 Module.register("MMM-BusTimesOBA", {
-
   defaults: {
     api_key: "TEST",
     stops: [],
-    exampleContent: ""
+    base_url: "https://api.pugetsound.onebusaway.org/",
   },
 
   /**
@@ -18,12 +17,17 @@ Module.register("MMM-BusTimesOBA", {
   /**
      * Pseudo-constructor for our module. Initialize stuff here.
      */
-  start() {
+  start: function () {
     Log.log("Starting module: " + this.name)
     this.templateContent = this.config.exampleContent
+    // Configure fields
+    this.arrival_details = null
+    this.stop_details = null
 
     // set timeout for next random text
-    setInterval(() => this.addRandomText(), 3000)
+    this.setConfiguration()
+    this.getArrivalAndDeparture()
+    setInterval(() => this.getArrivalAndDeparture(), 30000)
   },
 
   /**
@@ -34,8 +38,12 @@ Module.register("MMM-BusTimesOBA", {
      * @param {any} payload - The payload data`returned by the node helper.
      */
   socketNotificationReceived: function (notification, payload) {
-    if (notification === "EXAMPLE_NOTIFICATION") {
-      this.templateContent = `${this.config.exampleContent} ${payload.text}`
+    if (notification === "STOP_DETAILS") {
+      this.stop_details = payload.details
+      this.updateDom()
+    }
+    if (notification === "ARRIVAL_DETAILS") {
+      this.arrival_details = payload.details
       this.updateDom()
     }
   },
@@ -43,11 +51,50 @@ Module.register("MMM-BusTimesOBA", {
   /**
      * Render the page we're on.
      */
-  getDom() {
+  getDom: function () {
     const wrapper = document.createElement("div")
-    wrapper.innerHTML = `<b>Title</b><br />${this.templateContent}`
 
+    const stopInfo = document.createElement("div")
+    if (this.stop_details !== null) {
+      const stopData = document.createElement("ul")
+      this.config.stops.forEach((s) => {
+        const stopRow = document.createElement("li")
+        stopRow.innerHTML = `${this.stop_details[s].name} (${this.stop_details[s].direction})`
+        stopData.appendChild(stopRow)
+        if (this.arrival_details !== null) {
+          const schedule = document.createElement("ul")
+          for (const arrivalDetail of this.arrival_details[s]) {
+            const entry = document.createElement("li")
+            const bus = `${arrivalDetail.routeShortName} - ${arrivalDetail.tripHeadsign}`
+            let arrivalTime
+            if (arrivalDetail.predicted) {
+              arrivalTime = `${new Date(arrivalDetail.predictedArrivalTime).toLocaleTimeString()} predicted`
+            } else {
+              arrivalTime = ` ${new Date(arrivalDetail.scheduledArrivalTime).toLocaleTimeString()} scheduled`
+            }
+            entry.innerHTML = `${bus} : ${arrivalTime}`
+            schedule.appendChild(entry)
+          }
+          stopData.appendChild(schedule)
+        }
+      })
+      stopInfo.appendChild(stopData)
+    }
+
+    wrapper.appendChild(stopInfo)
     return wrapper
+  },
+
+  setConfiguration: function () {
+    this.sendSocketNotification("CONFIGURATION",
+      { api_key: this.config.api_key,
+        stops: this.config.stops,
+        base_url: this.config.base_url
+      })
+  },
+
+  getArrivalAndDeparture: function () {
+    this.sendSocketNotification("ARRIVAL_AND_DEPARTURE", {})
   },
 
   addRandomText() {
@@ -61,9 +108,9 @@ Module.register("MMM-BusTimesOBA", {
      * @param {number} payload the payload type.
      */
   notificationReceived(notification, payload) {
-    if (notification === "TEMPLATE_RANDOM_TEXT") {
-      this.templateContent = `${this.config.exampleContent} ${payload}`
-      this.updateDom()
-    }
+    // if (notification === "TEMPLATE_RANDOM_TEXT") {
+    //   this.templateContent = `${this.config.exampleContent} ${payload}`
+    //   this.updateDom()
+    // }
   }
 })
--- node_helper.js
+++ node_helper.js
@@ -1,29 +1,62 @@
 const NodeHelper = require("node_helper")
+const OneBusAway = require("onebusaway-sdk")
 
 module.exports = NodeHelper.create({
+  oba: undefined,
   start: function () {
     console.log("MMM-BusTimesOBA helper, started...")
 
     // Set up the local values
     this.base_url = ""
     this.api_key = ""
-    this.stops
+    this.stops = []
+    this.oba = null
+  },
 
-    this.location = ""
-    this.result = null
+  fetchStopDetails: async function () {
+    let result = {}
+    for (const stopId of this.stops) {
+      try {
+        const response = await this.oba.stop.retrieve(stopId)
+        result[stopId] = response.data.entry
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    return result
+  },
+
+  fetchArrivalDetails: async function () {
+    let result = {}
+    const query = {
+      minutesBefore: 5, // include vehicles having arrived or departed in the previous n minutes (default=5)
+      minutesAfter: 60, // include vehicles arriving or departing in the next n minutes (default=35)
+    }
+    for (const stopId of this.stops) {
+      try {
+        const response = await this.oba.arrivalAndDeparture.list(stopId, query)
+        result[stopId] = response.data.entry.arrivalsAndDepartures
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    return result
   },
 
   async socketNotificationReceived(notification, payload) {
     // if (notification === "EXAMPLE_NOTIFICATION") {}
-    if (notification === "GET_RANDOM_TEXT") {
-      const amountCharacters = payload.amountCharacters || 10
-      const randomText = Array.from({ length: amountCharacters }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join("")
-      this.sendSocketNotification("EXAMPLE_NOTIFICATION", { text: randomText })
-    }
     if (notification === "CONFIGURATION") {
-      const amountCharacters = payload.amountCharacters || 10
-      const randomText = Array.from({ length: amountCharacters }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join("")
-      this.sendSocketNotification("EXAMPLE_NOTIFICATION", { text: randomText })
+      this.api_key = payload.api_key
+      this.base_url = payload.base_url
+      this.stops = payload.stops
+      this.oba = new OneBusAway({ baseURL: this.base_url, apiKey: this.api_key })
+      const details = await this.fetchStopDetails()
+      this.sendSocketNotification("STOP_DETAILS", { details: details })
+    }
+
+    if (notification === "ARRIVAL_AND_DEPARTURE") {
+      const details = await this.fetchArrivalDetails()
+      this.sendSocketNotification("ARRIVAL_DETAILS", { details: details })
     }
   },
 })
